3deab2df2c80a748d13d0ee7f0f3d8389f3c684c
Adam Runner <adamrunner@gmail.com>
2015-12-30 00:15:04 -0800

    reload automatically, added todo note

--- lib/status.rb
+++ lib/status.rb
@@ -1,24 +1,28 @@
 module BackupServer
   class Status < Sinatra::Base
 
-    #configure do
-      # set app specific settings
-      # for example different view folders
-    #end
+
     set :logfile, '/var/log/designport_backup.sh.log'
+
     def is_backup_mounted?
       mounted = `mountpoint -q /media/usb && echo 'mounted' || echo 'false'`
       mounted =~ /mounted/
     end
 
+    def is_backup_running?
+      #TODO: determine if backup is currently running - lock file present etc.
+    end
+
     def tail_logfile
       IO.readlines(settings.logfile).reverse.join
     end
+
     get '/' do
       @backup_drive = is_backup_mounted?
       @log_file = tail_logfile
       erb :index
     end
+
     post '/stop_drive' do
       `sudo umount /media/usb`
       redirect '/'
